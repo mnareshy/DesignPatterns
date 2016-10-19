3f37c9adc9b142567670b7c5fc9a8a17c8e5f26c
sairam <sairam@sairam-Presario-CQ62-Notebook-PC>
2016-10-19 09:24:13 +0530

    commit

--- src/com/java8/fp/TestLamda.java
+++ src/com/java8/fp/TestLamda.java
@@ -3,18 +3,27 @@ package com.java8.fp;
 import java.util.function.Function;
 
 public class TestLamda {
-	
-	
-	static Function<Integer, Integer>	add = x -> x + 1 ;
-	
+
 	public static void main(String[] args) {
+
+		Function<Integer, Integer>	add = x -> x + 1 ;
+
+		System.out.println(add.apply(1));
+
+
+		Function<String, String>	concatinate = x -> x + 1 ;
+
+		System.out.println(concatinate.apply("2"));
 		
-	
-	
-	System.out.println(add.apply(1));
-		
+		Function<Integer, Integer> add1 = TestLamda::add1;
+		System.out.println(add1.apply(1));
 	}
 	
-	
+	public static Integer add1(Integer x)
+	{
+		return x+1;
+	}
+
+
 
 }
